1536c7b7f6bde06bd6c90127195fd88c3aaf4d39
skjesp <sebastian_kj@live.dk>
2020-04-30 10:20:53 +0200

    before major refactor

--- Project/AMS_GSM_MOD/AMS_GSM_MOD/communication.h
+++ Project/AMS_GSM_MOD/AMS_GSM_MOD/communication.h
@@ -7,6 +7,5 @@ char ReadChar(int uartNum);
 unsigned char CharReady(int uartNum);
 void SendChar(int uartNum, char c);
 void SendString(int uartNum, char* str);
-//int sendATcommand(char* ATcommand, int selected_UART, char* expectedResult);
 int sendATcommand(char* ATcommand, int selected_UART, char* expectedResult, char* resultBuffer);
 #endif
--- Project/AMS_GSM_MOD/AMS_GSM_MOD/main.c
+++ Project/AMS_GSM_MOD/AMS_GSM_MOD/main.c
@@ -18,10 +18,10 @@ int main()
 		return 1;
 	}
 	
-	// This function is OK
-	// fars telefonnr: +4523920863
-	// lasses tlf: +4522925623
-	
+	// This function is OK	
+	// johannes tlf: +4523923866
+	// lasse tlf: +4522925623
+	// lebara tlf: +4550238653
 	//SendSMS("SMS test is the best.", "+4522925623");
 	
 	
