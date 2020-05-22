da3ceecac9e4637df1c1789e619ff3a1a02148f4
lassesos <lazze95@live.dk>
2020-05-22 13:55:24 +0200

    it does not work

---
+++ Project/sg92rTest/sg92rTest/main.c
@@ -0,0 +1,22 @@
+/*
+ * sg92rTest.c
+ *
+ * Created: 14-05-2020 13:39:25
+ * Author : Lasse Kjærgaard
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 16000000
+#endif
+
+#include <util/delay.h>
+#include "sg92R.h"
+
+int main(void)
+{
+	SGInit();
+	//_delay_ms(500);
+	//RotateRight();
+	
+}
+
---
+++ Project/sg92rTest/sg92rTest/sg92r.c
@@ -0,0 +1,53 @@
+/*
+ * sg92r.c
+ *
+ * Created: 24-04-2020 13:36:42
+ *  Author: Lasse Kjærgaard
+ */
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "sg92R.h"
+
+void SGInit()
+{
+	DDRE |= (1<<PE4)|(1<<PE5); 
+	
+	TIMSK3 |= 0b00000001;
+	
+	TCCR3A|=(1<<COM3A1)|(1<<COM3B1)|(1<<WGM31);        //NON Inverted PWM
+	TCCR3B|=(1<<WGM33)|(1<<WGM32)|(1<<CS31)|(1<<CS30); //PRESCALER=64 MODE 14(FAST PWM)
+	ICR3=4999;  //fPWM=50Hz
+	
+	
+	TCNT3 = 0;
+	
+	OCR3A = 2000;
+	
+	sei();
+	
+}
+
+ISR(TIMER3_OVF_vect)
+{
+	OCR3A = 2000;
+}
+
+
+void RotateLeft()
+{
+	OCR3A = 250; //1ms	
+}
+
+void RotateRight()
+{
+	OCR3A = 500; //2ms		
+}
+
+void RotateMiddle()
+{
+	OCR3A = 375; //1,5ms	
+}
+ 
+
